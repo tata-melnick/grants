4070727a284723e3cb5b552f09648435a8580384
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-03-10 01:20:51 +0300

    Add type [types.ts]

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,10 @@
 export type GrantType = {
+    id: string
     name: string
     description: string
     provides: string
     status: string
     size: string
 }
+
+export type ListGrant = Array<GrantType>
