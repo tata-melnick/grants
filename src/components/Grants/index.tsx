799f7a2ccecfd8992ccb616383782835458eafb7
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-03-09 23:37:11 +0300

    Add component [Grant] (ListGrant)

--- src/components/Grants/index.tsx
+++ src/components/Grants/index.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import styles from "./grants.module.css"
 import Search from "../Search"
+import ListGrant from "../ListGrant"
 
 const Grants: React.FC = () => {
     return (
         <div className={styles.content}>
             <Search />
-            {/*<ListGrants />*/}
+            <ListGrant />
             {/*<Paginator/>*/}
         </div>
     )
