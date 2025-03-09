2cb6c69d961c99ad97313c5385461a214afef0c6
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-03-10 01:21:54 +0300

    Add component Paginator [ListGrant]

--- src/components/ListGrant/index.tsx
+++ src/components/ListGrant/index.tsx
@@ -1,15 +1,41 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styles from "./listGrant.module.css"
 import Grant from "./Grant"
 import GRANTS from "../../mock/grants.json"
+import Paginator from "../Paginator"
+import { ListGrant } from "../../types"
 
-const ListGrant: React.FC = () => {
+interface IListGranProps {
+    grants: ListGrant
+}
+
+const count = 3
+
+const ListGrant: React.FC<IListGranProps> = ({ grants }) => {
+    const [page, setPage] = useState<number>(0)
+    const [grantsOnPage, setGrantsOnPage] = useState<ListGrant>([])
+    grants = GRANTS
+
+    console.log(grants)
+
+    useEffect(() => {
+        setGrantsOnPage(grants.slice(page * count, page * count + count))
+    }, [page, grants])
+
     return (
-        <div className={styles.list}>
-            {GRANTS.map((item) => (
-                <Grant detail={item} key={item.id} />
-            ))}
-        </div>
+        <>
+            <div className={styles.list}>
+                {grantsOnPage &&
+                    grantsOnPage.map((item) => (
+                        <Grant detail={item} key={item.id} />
+                    ))}
+            </div>
+            <Paginator
+                page={page}
+                onChange={setPage}
+                count={Math.ceil(grants.length / count)}
+            />
+        </>
     )
 }
 
