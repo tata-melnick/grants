65236358e1dc431c310016884e7fdb284dfc034b
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-03-17 15:57:35 +0300

    Add header change and data substitution when switching to a detailed page [Header]

--- src/Layout/components/Header.tsx
+++ src/Layout/components/Header.tsx
@@ -1,17 +1,39 @@
 import React from "react"
 import styles from "./styles.module.css"
+import Grants from "../../store/grant"
+import { observer } from "mobx-react-lite"
+import { useLocation } from "react-router-dom"
 
-const Header: React.FC = () => {
+const Header: React.FC = observer(() => {
+    const { grant } = Grants
+    const location = useLocation()
+    const pathname = location.pathname
+
     return (
         <div className={styles.header}>
-            <h1 className={styles.title}>
-                Подбери программу для реализации своего проекта
-            </h1>
-            <div className={styles.img}>
-                <img src="/public/group.png" alt="" />
-            </div>
+            {pathname !== "/" ? (
+                <>
+                    <h1 className={styles.titleDetail}>{grant?.name}</h1>
+                    <div className={styles.info}>
+                        <div className={styles.provides}>
+                            <div>Организатор конкурса:</div>
+                            <div className={styles.text}>{grant?.provides}</div>
+                        </div>
+                        <button className={styles.btn}>Перейти на сайт</button>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <h1 className={styles.title}>
+                        Подбери программу для реализации своего проекта
+                    </h1>
+                    <div className={styles.img}>
+                        <img src="/public/group.png" alt="" />
+                    </div>
+                </>
+            )}
         </div>
     )
-}
+})
 
 export default Header
