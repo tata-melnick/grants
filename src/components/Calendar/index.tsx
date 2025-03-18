81d91f52610c48449331060217f8b5a1a9d4bad6
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-03-18 11:53:35 +0300

    Add component Calendar [Calendar, index.css] (Filters)

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react"
+import { Calendar as LibCalendar } from "react-calendar"
+import { Range, Value } from "react-calendar/src/shared/types"
+import cn from "classnames-ts"
+import styles from "./calendar.module.css"
+import "../../index.css"
+
+interface ICalendarProps {
+    type?: "range" | "standard"
+    onChange?(value: Date | null): void
+    onRangeChange(value: Range<Date | null>): (void | undefined) | undefined
+}
+
+const formatDate = (date: Date | null) => {
+    if (!date) return "-"
+
+    const { format } = new Intl.DateTimeFormat("ru-RU", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+    })
+
+    return format(date).replaceAll(".", "/")
+}
+
+export const Calendar = ({
+    type = "standard",
+    onChange,
+    onRangeChange,
+}: ICalendarProps) => {
+    const [start, setStart] = useState<Date | null>(null)
+    const [end, setEnd] = useState<Date | null>(null)
+
+    const handleChange = (value: Value) => {
+        if (Array.isArray(value)) {
+            setStart(value[0])
+            setEnd(value[1])
+            onRangeChange && onRangeChange(value)
+        } else {
+            setStart(value)
+            onChange && onChange(value)
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            {type === "range" && (
+                <div className={styles.rangeContainer}>
+                    <div className={styles.range}>
+                        <div className={styles.rangeTitle}>Начало периода</div>
+                        <div
+                            className={cn(
+                                styles.rangeDate,
+                                start && styles.active
+                            )}
+                        >
+                            <img src="/public/calendar.svg" alt="" />
+                            <span className={styles.date}>
+                                {formatDate(start)}
+                            </span>
+                        </div>
+                    </div>
+                    <div className={styles.range}>
+                        <div className={styles.rangeTitle}>Конец периода</div>
+                        <div
+                            className={cn(
+                                styles.rangeDate,
+                                end && styles.active
+                            )}
+                        >
+                            <img src="/public/calendar.svg" alt="" />
+                            <span className={styles.date}>
+                                {formatDate(end)}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            )}
+            <div className={styles.calendarContainer}>
+                <LibCalendar
+                    locale="ru-Ru"
+                    className={styles.calendar}
+                    onChange={handleChange}
+                    navigationLabel={({ date, locale }) =>
+                        date.toLocaleDateString(locale, { month: "long" })
+                    }
+                    value={type === "range" ? [start, end] : start}
+                    selectRange={type === "range"}
+                    returnValue={type === "range" ? "range" : "start"}
+                    navigationAriaLabel="polite"
+                    showNeighboringMonth={false}
+                />
+            </div>
+        </div>
+    )
+}
