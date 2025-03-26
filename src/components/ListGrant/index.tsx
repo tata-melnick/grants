f976a02b00bc9141d11d7841af1c64f939b0ff70
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-03-26 12:47:00 +0300

    Add display of the count of grants depending on the screen resolution [ListGrant]

--- src/components/ListGrant/index.tsx
+++ src/components/ListGrant/index.tsx
@@ -5,17 +5,22 @@ import Paginator from "../Paginator"
 import { ListGrant } from "../../types"
 import Grants from "../../store/grant"
 import { observer } from "mobx-react-lite"
+import UiState from "../../store/uiState"
 
-const count = 3
+let count: number = 3
 
 const ListGrant: React.FC = observer(() => {
     const { list } = Grants
+    const { isDesktop, isTablet, isMobile, screenType } = UiState
     const [page, setPage] = useState<number>(0)
     const [grantsOnPage, setGrantsOnPage] = useState<ListGrant>([])
 
     useEffect(() => {
+        if (isDesktop) count = 3
+        if (isTablet) count = 4
+        if (isMobile) count = 3
         setGrantsOnPage(list.slice(page * count, page * count + count))
-    }, [page, list])
+    }, [page, list, screenType])
 
     return (
         <>
