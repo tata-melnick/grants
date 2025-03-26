4f6b7e57d74fa5216f60ef0240f3f7387bbdd987
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-03-26 12:52:28 +0300

    Add movement of components depending on screen resolution [BriefInfo]

--- src/pages/DetailPage/components/BriefInfo/index.tsx
+++ src/pages/DetailPage/components/BriefInfo/index.tsx
@@ -1,32 +1,60 @@
 import React from "react"
+import UiState from "../../../../store/uiState"
+import cn from "classnames-ts"
 import styles from "./briefInfo.module.css"
 import { GrantType } from "../../../../types"
+import { MenuIcon } from "../../../../icons"
+import modal from "../../../../store/modal"
+import { observer } from "mobx-react-lite"
 
 interface IBriefInfoProps {
     grant: GrantType
     id?: string
 }
 
-const BriefInfo: React.FC<IBriefInfoProps> = ({ grant }) => {
+const BriefInfo: React.FC<IBriefInfoProps> = observer(({ grant }) => {
     const { projectStage, status, sum, endTime } = grant
+    const { isMobile } = UiState
 
     return (
         <div id="briefInfo">
             <h3 className={styles.title}>Краткая информация</h3>
             <div className={styles.briefInfo}>
-                <div className={styles.condition}>
-                    Статус конкурса:
-                    <br />
-                    {projectStage}
+                {isMobile && (
+                    <div className={cn(styles.condition, styles.conditionOne)}>
+                        Статус конкурса: {projectStage}
+                    </div>
+                )}
+                {!isMobile && (
+                    <div className={styles.condition}>
+                        Статус конкурса:
+                        <br />
+                        {projectStage}
+                    </div>
+                )}
+                <div className={cn(styles.condition, styles.conditionTwo)}>
+                    Срок выполнения гранта:
                 </div>
-                <div className={styles.condition}>Срок выполнения гранта:</div>
-                <div className={styles.condition}>Размер гранта:</div>
-                <div className={styles.text}>Подача заявок до {status.to}</div>
-                <div className={styles.text}>{endTime} месяцев</div>
-                <div className={styles.text}>{sum} млн. руб.</div>
+                <div className={cn(styles.condition, styles.conditionThree)}>
+                    Размер гранта:
+                </div>
+                <div className={cn(styles.text, styles.textOne)}>
+                    Подача заявок до {status.to}
+                </div>
+                <div className={cn(styles.text, styles.textTwo)}>
+                    {endTime} месяцев
+                </div>
+                <div className={cn(styles.text, styles.textThree)}>
+                    {sum} млн. руб.
+                </div>
+                {isMobile && (
+                    <button className={styles.btn} onClick={modal.open}>
+                        <MenuIcon />
+                    </button>
+                )}
             </div>
         </div>
     )
-}
+})
 
 export default BriefInfo
