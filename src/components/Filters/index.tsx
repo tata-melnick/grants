b42a50b5593e6c4c5667bd15e1fc4e0fa8d1e336
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-03-26 12:41:07 +0300

    Moved buttons to component MainPage

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -6,65 +6,57 @@ import { Calendar } from "../Calendar"
 
 const Filters: React.FC = () => {
     return (
-        <div className={styles.content}>
-            <div className={styles.wrap}>
-                <button className={styles.btnText}>Фильтр</button>
-                <div className={styles.btnWrap}>
-                    <button className={styles.btn}>Сбросить</button>
+        <div className={styles.sections}>
+            <div className={styles.section}>
+                <h3 className={styles.titleSection}>Стадия проекта</h3>
+                <div className={styles.filter}>
+                    <Radio />
                 </div>
             </div>
-            <div className={styles.sections}>
-                <div className={styles.section}>
-                    <h3 className={styles.titleSection}>Стадия проекта</h3>
-                    <div className={styles.filter}>
-                        <Radio />
-                    </div>
+            <div className={styles.section}>
+                <h3 className={styles.titleSection}>Регион участия</h3>
+                <div className={styles.filter}>
+                    <Checkbox />
                 </div>
-                <div className={styles.section}>
-                    <h3 className={styles.titleSection}>Регион участия</h3>
-                    <div className={styles.filter}>
-                        <Checkbox />
-                    </div>
+            </div>
+            <div className={styles.section}>
+                <h3 className={styles.titleSection}>Направление проекта</h3>
+                <div className={styles.filter}>
+                    <Checkbox />
                 </div>
-                <div className={styles.section}>
-                    <h3 className={styles.titleSection}>Направление проекта</h3>
-                    <div className={styles.filter}>
-                        <Checkbox />
-                    </div>
+            </div>
+            <div className={styles.section}>
+                <h3 className={styles.titleSection}>Сумма гранта</h3>
+                <div className={styles.filter}>
+                    <Checkbox />
                 </div>
-                <div className={styles.section}>
-                    <h3 className={styles.titleSection}>Сумма гранта</h3>
-                    <div className={styles.filter}>
-                        <Checkbox />
-                    </div>
+            </div>
+            <div className={styles.section}>
+                <h3 className={styles.titleSection}>
+                    Правовая форма грантополучателя
+                </h3>
+                <div className={styles.filter}>
+                    <Radio />
                 </div>
-                <div className={styles.section}>
-                    <h3 className={styles.titleSection}>
-                        Правовая форма грантополучателя
-                    </h3>
-                    <div className={styles.filter}>
-                        <Radio />
-                    </div>
+            </div>
+            <div className={styles.section}>
+                <h3 className={styles.titleSection}>Возраст участников</h3>
+                <div className={styles.filter}>
+                    <Radio />
                 </div>
-                <div className={styles.section}>
-                    <h3 className={styles.titleSection}>Возраст участников</h3>
-                    <div className={styles.filter}>
-                        <Radio />
-                    </div>
+            </div>
+            <div className={styles.section}>
+                <h3 className={styles.titleSection}>
+                    Только для преподавателей
+                </h3>
+                <div className={styles.filter}>
+                    <Checkbox />
                 </div>
-                <div className={styles.section}>
-                    <h3 className={styles.titleSection}>
-                        Только для преподавателей
-                    </h3>
-                    <div className={styles.filter}>
-                        <Checkbox />
-                    </div>
-                </div>
+            </div>
 
-                <div className={styles.section}>
-                    <h3 className={styles.titleSection}>Название раздела</h3>
-                    <Calendar type="range" />
-                </div>
+            <div className={styles.section}>
+                <h3 className={styles.titleSection}>Название раздела</h3>
+                <Calendar type="range" />
             </div>
         </div>
     )
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,14 +1,38 @@
 import React from "react"
 import styles from "./mainPage.module.css"
 import { Filters, Grants } from "../../components"
+import { observer } from "mobx-react-lite"
+import UiState from "../../store/uiState"
+import Popup from "../../components/Popup"
+import PopupFilters from "../../store/popup"
 
-const MainPage: React.FC = () => {
+const MainPage: React.FC = observer(() => {
+    const { isMobile } = UiState
+    const { popup, openPopup, closePopup } = PopupFilters
+
     return (
         <div className={styles.content}>
-            <Filters />
+            <div className={styles.filters}>
+                <div className={styles.wrap}>
+                    <button onClick={openPopup} className={styles.btnText}>
+                        Фильтр
+                    </button>
+                    <div className={styles.btnWrap}>
+                        <button onClick={closePopup} className={styles.btn}>
+                            Сбросить
+                        </button>
+                    </div>
+                </div>
+                {!isMobile && <Filters />}
+                {isMobile && (
+                    <Popup open={popup}>
+                        <Filters />
+                    </Popup>
+                )}
+            </div>
             <Grants />
         </div>
     )
-}
+})
 
 export default MainPage
