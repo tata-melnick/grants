508e40f5a9ee5d8d4eacd35aa5e8e244fc6eb5b3
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-04-15 16:53:26 +0300

    Build app

--- src/helpers/randomStr.ts
+++
@@ -1,8 +0,0 @@
-const rndStr = (length?: number) => {
-    let rnd = ""
-    while (rnd.length < (length || 4))
-        rnd += Math.random().toString(36).substring(2)
-    return rnd.substring(0, length || 4)
-}
-
-export default rndStr
--- src/modals/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as FiltersModal } from "./FiltersModal"
-export { default as MenuModal } from "./MenuModal"
